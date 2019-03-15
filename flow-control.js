737df14aa8426759c144251e95fe1144218d24e0
Zach Mobilian <zach.mobilian@blackrock.com>
2019-03-15 15:01:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,12 @@ function ageChecker(age) {
 
 
 function ternaryTeenager(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!";
 }
+    return "You are not a teenager";
+}
+
 
 function switchAge(age) {
 
